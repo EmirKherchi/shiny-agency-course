1934944e2f76ee02990ec22daa249215fe2d6da5
Emir <contact@ekherchi.fr>
2022-09-22 10:27:58 +0200

    fetch data from api and use it with hooks

--- src/Pages/Freelances/index.jsx
+++ src/Pages/Freelances/index.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react'
 import Card from '../../Components/Card/index'
 import styled from 'styled-components'
 import color from '../../utils/style/color'
+import { Loader } from '../../utils/style/atom'
 
 const CardsBaseline = styled.div`
   text-align: center;
@@ -20,47 +22,81 @@ const CardsContainer = styled.div`
   display: grid;
   align-items: center;
   justify-items: center;
-  gap: 24px;
+  gap: 32px;
   grid-template-rows: repeat(2, 1fr);
   grid-template-columns: repeat(2, 1fr);
   margin: 1rem;
 `
 
-const freelanceProfiles = [
-  {
-    name: 'Jane Doe',
-    jobTitle: 'Devops',
-  },
-  {
-    name: 'John Doe',
-    jobTitle: 'Developpeur frontend',
-  },
-  {
-    name: 'Jeanne Biche',
-    jobTitle: 'Développeuse Fullstack',
-  },
-  {
-    name: 'Jeanne Biche',
-    jobTitle: 'Développeuse Fullstack',
-  },
-]
+// const freelanceProfiles = [
+//   {
+//     name: 'Jane Doe',
+//     jobTitle: 'Devops',
+//   },
+//   {
+//     name: 'John Doe',
+//     jobTitle: 'Developpeur frontend',
+//   },
+//   {
+//     name: 'Jeanne Biche',
+//     jobTitle: 'Développeuse Fullstack',
+//   },
+//   {
+//     name: 'Jeanne Biche',
+//     jobTitle: 'Développeuse Fullstack',
+//   },
+// ]
 
 function Freelances() {
+  const [freelanceProfiles, setFreelanceProfiles] = useState([])
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [fetchError, setFetchError] = useState(false)
+
+  useEffect(() => {
+    async function getFreelanceProfiles() {
+      setDataLoading(true)
+      try {
+        const response = await fetch('http://localhost:8000/freelances')
+        const data = await response.json()
+        console.log(data.freelancersList)
+        setFreelanceProfiles(data.freelancersList)
+        // const { freelancersList } = await response.data
+        // console.log(freelancersList)
+        // setFreelanceProfiles(await freelancersList)
+      } catch (error) {
+        setFetchError(true)
+        console.log(error)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    getFreelanceProfiles()
+  }, [])
+
   return (
     <div>
       <CardsBaseline>
         <h1>Trouvez votre prestataire</h1>
         <span>Chez Shiny nous réunissons les meilleurs profils pour vous.</span>
       </CardsBaseline>
-      <CardsContainer>
-        {freelanceProfiles.map((profile, index) => (
-          <Card
-            key={`${profile.name}-${index}`}
-            label={profile.jobTitle}
-            title={profile.name}
-          />
-        ))}
-      </CardsContainer>
+      {isDataLoading || fetchError ? (
+        isDataLoading ? (
+          <Loader />
+        ) : (
+          <p>There is an error</p>
+        )
+      ) : (
+        <CardsContainer>
+          {freelanceProfiles.map((profile, index) => (
+            <Card
+              key={profile.id}
+              label={profile.job}
+              title={profile.name}
+              picture={profile.picture}
+            />
+          ))}
+        </CardsContainer>
+      )}
     </div>
   )
 }
--- src/Pages/Survey/index.jsx
+++ src/Pages/Survey/index.jsx
@@ -1,35 +1,88 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import color from '../../utils/style/color'
+import { Loader } from '../../utils/style/atom'
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${color.primary};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
 
 function Survey() {
   const { questionNumber } = useParams()
+  const questionNumberInt = parseInt(questionNumber)
+  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
+  const nextQuestionNumber = questionNumberInt + 1
+  const [surveyData, setSurveyData] = useState({})
+  const [isDataLoading, setDataLoading] = useState(false)
+
+  // useEffect(() => {
+  //   setDataLoading(true)
+  //   fetch('http://localhost:8000/survey')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setSurveyData(data.surveyData)
+  //       setDataLoading(false)
+  //     })
+  //     .catch((error) => console.log(error))
+  // }, [])
+
+  useEffect(() => {
+    async function getSurveyData() {
+      setDataLoading(true)
+      try {
+        const response = await fetch('http://localhost:8000/survey')
+        const { surveyData } = await response.json()
+        setSurveyData(await surveyData)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    getSurveyData()
+  }, [])
+
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <li>
-        <Link
-          to={
-            parseFloat(questionNumber) < 2
-              ? '/survey/1'
-              : `/survey/${parseFloat(questionNumber) - 1}`
-          }
-        >
-          Previous Question
-        </Link>
-      </li>
-      <li>
-        <Link
-          to={
-            parseFloat(questionNumber) !== 10
-              ? `/survey/${parseFloat(questionNumber) + 1}`
-              : '/results'
-          }
-        >
-          Next Question
-        </Link>
-      </li>
-      <h2>Question numero {questionNumber}</h2>
-    </div>
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+      )}
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   )
 }
 export default Survey
